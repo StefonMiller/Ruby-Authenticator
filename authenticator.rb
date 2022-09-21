88a3091925d6b39ec38639432ae21028836bbcd5
unknown <stefonplays@gmail.com>
2022-09-20 22:49:25 -0400

    Added documentation

--- authenticator.rb
+++ authenticator.rb
@@ -3,12 +3,25 @@
 # the full user record. The user has 3 tries to enter
 # valid login credentials before the program exits
 
+# Predefined users
 users = [
     {username: "Stefon", password: "cats33"},
     {username: "Emily", password: "bean"},
     {username: "Barb", password: "smoke"},
     {username: "Mike", password: "beer"}
 ]
+
+# Method to validate users, returns Nil if no valid user found
+def validate_credentials(un, pw, users)
+    valid_user = nil
+    # Validate information
+    users.each do |user|
+        if user[:username] == un && user[:password] == pw
+            valid_user = user
+        end
+    end
+    valid_user
+end
 
 # Print welcome screen
 puts "Welcome to my authenticator"
@@ -17,27 +30,25 @@ puts
 
 
 tries = 0
-
-while tries <+ 3
+# Loop until tries exceeded
+while true
     # Increment tries on every iteration
     tries += 1
 
+    # Prompt for info
     puts "Please enter your username"
     user_name = gets.chomp
     puts "Please enter your password"
     pw = gets.chomp
-    valid_user = nil
-    users.each do |user|
-        if user[:username] == user_name && user[:password] == pw
-            valid_user = user
-        end
-    end
-    if valid_user.nil?
+    user = validate_credentials(user_name, pw, users)
+    # Check if validation retrieved valid user
+    if user.nil?
         puts "Invalid credentials"
     else
-        puts valid_user
+        puts user
         break
     end
+    # Exit if over tries or if user elects to
     break if tries == 3
     puts "Press q to quit. Press any other key to try again:"
     continue = gets.chomp
